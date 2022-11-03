c65acdb5742a5bc37196d3f8c6ce4300e326828c
Antoninstolfa <97514759+Antoninstolfa@users.noreply.github.com>
2022-11-03 13:19:46 +0100

    Add files via upload

---
+++ Datum a čas Test/Datum a čas Test/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Datum_a_čas_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tvorba rodného čísla";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(44, 49);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(44, 135);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(48, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "žena";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(44, 158);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(44, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "muž";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Vytvoř rodné číslo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(373, 49);
+            this.maskedTextBox1.Mask = "000000\\/000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(124, 20);
+            this.maskedTextBox1.TabIndex = 5;
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(373, 96);
+            this.maskedTextBox2.Mask = "000000\\/0000";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(124, 20);
+            this.maskedTextBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(370, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Výsledky";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(286, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Před 1.1. 1954:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(286, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Po 1.1. 1954:";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(373, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Celkem";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(678, 369);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(97, 69);
+            this.listBox1.TabIndex = 11;
+            this.listBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Rodné číslo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Datum a čas Test/Datum a čas Test/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Datum_a_čas_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public DateTime DatumNarozeni = new DateTime();
+        public DateTime zmenaRC = new DateTime();
+        public int rok, mesic, den;
+        public int cislo1, cislo2, cislo3, cislo4;
+        public int pocitadloZena = 0, pocitadloMuz = 0, pocetRC = 0;
+        public bool konec = false;
+        public bool zmena = false;
+        public Random rng = new Random();
+        public string Rcislo1 = "";
+        public string Rcislo2 = "";
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            maskedTextBox1.Text = "000000000";
+            maskedTextBox2.Text = "0000000000";
+            DatumNarozeni = dateTimePicker1.Value;
+            rok = DatumNarozeni.Year;
+            mesic = DatumNarozeni.Month;
+            den = DatumNarozeni.Day;
+            rok = rok % 100;
+            cislo1 = rng.Next(1,9);
+            cislo2 = rng.Next(1, 9);
+            cislo3 = rng.Next(1, 9);
+            cislo4 = rng.Next(1, 9);
+            if(zmena == false)
+            {
+                zmenaRC = zmenaRC.AddYears(1953);
+                zmena = true;
+            }
+            label1.Text = zmenaRC.ToString();
+
+            if (radioButton1.Checked)
+            {
+                mesic = mesic + 50;
+                pocitadloZena++;
+                pocetRC++;
+                konec = false;
+            }
+            else if (radioButton2.Checked)
+            {
+                pocitadloMuz++;
+                pocetRC++;
+                konec = false;
+            }
+            else
+            {
+                MessageBox.Show("Vyplňte Všechyn informace!" + "Chyba!");
+                konec = true;
+            }
+            if(pocetRC>=2)
+            {
+                button2.Visible = true;
+                button2.Enabled = true;
+            }
+
+            if (DatumNarozeni < zmenaRC && konec == false)
+            {
+                if (rok < 10)
+                {
+                    if (mesic < 10)
+                    {
+                        if (den < 10)
+                        {
+                            Rcislo1 = "0" + rok.ToString() + "0" + mesic.ToString() + "0" + den.ToString() + cislo1 + cislo2 + cislo3;
+                        }
+                        else
+                        {
+                            Rcislo1 = "0" + rok.ToString() + "0" + mesic.ToString() + den.ToString() + cislo1 + cislo2 + cislo3;
+                        }
+                    }
+                    else
+                    {
+                        if (den < 10)
+                        {
+                            Rcislo1 = "0" + rok.ToString() + mesic.ToString() + "0" + den.ToString() + cislo1 + cislo2 + cislo3;
+                        }
+                        else
+                        {
+                            Rcislo1 = "0" + rok.ToString() + mesic.ToString() + den.ToString() + cislo1 + cislo2 + cislo3;
+                        }
+                    }
+                }
+                else
+                {
+                    if (mesic < 10)
+                    {
+                        if (den < 10)
+                        {
+                            Rcislo1 = rok.ToString() + "0" + mesic.ToString() + "0" + den.ToString() + cislo1 + cislo2 + cislo3;
+                        }
+                        else
+                        {
+                            Rcislo1 = rok.ToString() + "0" + mesic.ToString() + den.ToString() + cislo1 + cislo2 + cislo3;
+                        }
+                    }
+                    else
+                    {
+                        if (den < 10)
+                        {
+                            Rcislo1 = rok.ToString() + mesic.ToString() + "0" + den.ToString() + cislo1 + cislo2 + cislo3;
+                        }
+                        else
+                        {
+                            Rcislo1 = rok.ToString() + mesic.ToString() + den.ToString() + cislo1 + cislo2 + cislo3;
+                        }
+                    }
+                }
+                listBox1.Items.Add(Rcislo1);
+            }
+            else if(DatumNarozeni > zmenaRC && konec == false)
+            {
+                if (rok < 10)
+                {
+                    if (mesic < 10)
+                    {
+                        if (den < 10)
+                        {
+                            Rcislo2 = "0" + rok.ToString() + "0" + mesic.ToString() + "0" + den.ToString() + cislo1 + cislo2 + cislo3 + cislo4;
+                        }
+                        else
+                        {
+                            Rcislo2 = "0" + rok.ToString() + "0" + mesic.ToString() + den.ToString() + cislo1 + cislo2 + cislo3 + cislo4;
+                        }
+                    }
+                    else
+                    {
+                        if (den < 10)
+                        {
+                            Rcislo2 = "0" + rok.ToString() + mesic.ToString() + "0" + den.ToString() + cislo1 + cislo2 + cislo3 + cislo4;
+                        }
+                        else
+                        {
+                            Rcislo2 = "0" + rok.ToString() + mesic.ToString() + den.ToString() + cislo1 + cislo2 + cislo3 + cislo4;
+                        }
+                    }
+                }
+                else
+                {
+                    if (mesic < 10)
+                    {
+                        if (den < 10)
+                        {
+                            Rcislo2 = rok.ToString() + "0" + mesic.ToString() + "0" + den.ToString() + cislo1 + cislo2 + cislo3 + cislo4;
+                        }
+                        else
+                        {
+                            Rcislo2 = rok.ToString() + "0" + mesic.ToString() + den.ToString() + cislo1 + cislo2 + cislo3 + cislo4;
+                        }
+                    }
+                    else
+                    {
+                        if (den < 10)
+                        {
+                            Rcislo2 = rok.ToString() + mesic.ToString() + "0" + den.ToString() + cislo1 + cislo2 + cislo3 + cislo4;
+                        }
+                        else
+                        {
+                            Rcislo2 = rok.ToString() + mesic.ToString() + den.ToString() + cislo1 + cislo2 + cislo3 + cislo4;
+                        }
+                    }
+                }
+                listBox1.Items.Add(Rcislo2);
+            }
+            maskedTextBox1.Text = Rcislo1;
+            maskedTextBox2.Text = Rcislo2;
+        }
+
+        public int Nejmladsi;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Počet žen: " + pocitadloZena + "\nPočet Mužů: " + pocitadloMuz);
+        }
+    }
+}
